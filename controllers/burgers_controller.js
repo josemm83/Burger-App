0f7997a0dac453aab03ad403d9c224500fc012a0
josemm83 <josemmancha@gmail.com>
2020-03-12 13:30:59 -0500

    updated files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+var burger = require("../models/burger");
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    cat.create([
+      "name", "sleepy"
+    ], [
+    //   req.body.name, req.body.sleepy
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+module.exports = router;

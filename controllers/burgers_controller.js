9e19559499c8c608c935cc92556615f5001aecf7
josemm83 <josemmancha@gmail.com>
2020-03-14 19:03:59 -0500

    updating files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,9 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    cat.all(function(data) {
+    burger.all(function(data) {
       var hbsObject = {
-        cats: data
+        burgers: data
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
@@ -14,26 +14,21 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    cat.create([
-      "name", "sleepy"
+    burger.create([
+      "burger_name", "devoured"
     ], [
-    //   req.body.name, req.body.sleepy
     ], function(result) {
-      // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
   router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
-  
     console.log("condition", condition);
-  
-    cat.update({
-      sleepy: req.body.sleepy
+    burger.update({
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
